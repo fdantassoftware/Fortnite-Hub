2ca4a3b6b33f9ba9b8ce4ebf48f23b6a3fa42c48
Fabio Dantas <dantasirl@hotmail.com>
2018-10-09 10:10:12 +0100

    Initial Commit

---
+++ Fortnite Hub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fortnite Hub
+//
+//  Created by Fabio Dantas on 09/10/2018.
+//  Copyright © 2018 Fabio Dantas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
